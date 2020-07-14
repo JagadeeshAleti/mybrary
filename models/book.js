521addb3d5ff457e2fb78e371cfc8a5f398948ce
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-07-14 12:27:42 +0530

    finishing touch for books route

--- models/book.js
+++ models/book.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const path = require('path')
+const coverImageBasePath = 'upoads/bookCovers'
 
-const Book = mongoose.model('Book', {
+const bookSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true
@@ -30,7 +32,13 @@ const Book = mongoose.model('Book', {
         required: true,
         ref: 'Author'
     }
-
 })
 
-module.exports = Book
+bookSchema.virtual('coverImagePath').get(function() {
+    if (this.coverImageName != null) {
+        return path.join('/', coverImageBasePath, this.coverImageName)
+    }
+})
+
+module.exports = mongoose.model('Book', bookSchema)
+module.exports.coverImageBasePath = coverImageBasePath  
